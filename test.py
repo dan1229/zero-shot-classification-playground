0e16928ae9739a89cb1a79eac8fda5e9ef89e355
Daniel Nazarian <danielnazarian@outlook.com>
2024-03-24 03:07:30 -0400

    test stat improements

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ from typing import Optional
 from main import METHODS, classify_image
 
 
-# This is structured as follows:
+# Data structure to store test results
+# Format:
 # {
 #     "image-url": {
 #         "method": {
-#             "score": score,
+#             "scores": scores,
 #             "time": time_elapsed,
 #             "category": category
 #         }
-#
 #     }
 # }
 test_image_method_scores = {}
@@ -31,24 +31,29 @@ TEST_URLS = [
 ]
 
 
-def _get_higest_scores(scores, num: Optional[int] = 3) -> list:
+def _get_highest_scores(scores, num: Optional[int] = 3) -> list:
     highest_scores = []
     for obj in scores:
         highest_scores.append(obj["score"])
-    return sorted(highest_scores)[:num]
+    return sorted(highest_scores, reverse=True)[:num]
+
+
+def _get_label_for_score(scores: list, score: float) -> str:
+    for obj in scores:
+        if obj["score"] == score:
+            return obj["label"]
+    return "N/A"
 
 
 if __name__ == "__main__":
     start_time = time.time()
     tests_run = 0
-    shortest_time = float("inf")
-    fastest_method = None
 
     try:
-        # Loop through each test url
-        for index, url in enumerate(TEST_URLS):
+        # Loop through each test URL
+        for index, url in enumerate(TEST_URLS, start=1):
             print("\n** ===================================================== **")
-            print(f"Test {index + 1}")
+            print(f"Test {index}")
             print(f"Testing URL: {url}\n")
 
             # Loop through each method
@@ -57,6 +62,7 @@ if __name__ == "__main__":
                 if method == "Specialized Categories":
                     print("\t\tSkipping method SPECIALIZED_CATEGORIES")
                     continue
+
                 # Run classify_image function with the current method
                 print(f"\tclassify_image({url}, {method})")
                 scores, time_elapsed, category = classify_image(url, method)
@@ -69,28 +75,47 @@ if __name__ == "__main__":
                     "time": time_elapsed,
                     "category": category,
                 }
-            tests_run += 1
+                tests_run += 1
 
         # Print overall stats
         print("\n=============================")
-        print("\nOverall Stats:")
+        print("\nIndividual Test Stats:")
         for url, method_obj in test_image_method_scores.items():
+            shortest_time = None
+            fastest_method = None
+            highest_scoring_method = None
+            highest_scoring_label = None
+            highest_score = None
             print(f"\nURL: {url}")
             for method, obj in method_obj.items():
+                highest_scores = _get_highest_scores(obj["scores"])
                 print(f"\tMethod: {method}")
-                print(f"\t\tScore: {_get_higest_scores(obj['scores'])}")
+                print(f"\t\tScore: {highest_scores}")
                 print(f"\t\tCategory: {obj['category']}")
                 print(f"\t\tElapsed Time: {obj['time']} seconds")
-                if obj["time"] < shortest_time:
+                for tmp in highest_scores:
+                    if not highest_score or tmp > highest_score:
+                        highest_score = tmp
+                        highest_scoring_method = method
+                        highest_scoring_label = _get_label_for_score(obj["scores"], tmp)
+                if not shortest_time or obj["time"] < shortest_time:
                     shortest_time = obj["time"]
                     fastest_method = method
+            print("\tWINNER")
+            print("\t\tFastest Method:", fastest_method)
+            print("\t\t\tShortest Time:", shortest_time, "seconds")
+            print("\t\tHighest Scoring Method:", highest_scoring_method)
+            print("\t\t\tHighest Scoring Label:", highest_scoring_label)
+            print("\t\t\tHighest Score:", highest_score)
     except Exception as e:
         print("Error:", e)
     print("")
-    print(f"\nFastest Method: {fastest_method}")
-    print(f"\tElapsed Time: {shortest_time} seconds")
+    print("\n=============================")
+    print("\nOverall Results:")
+    print("URLS tested:", len(TEST_URLS))
     print("Tests run:", tests_run)
-    print("")
-    print("Testing complete.")
     end_time = time.time()
     print("Total time elapsed:", end_time - start_time, "seconds")
+    print("")
+    print("Testing complete.")
+    print("\n=============================")
