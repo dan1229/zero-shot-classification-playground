0689472ec514617ac2d37ac0a8258849379d95b8
Daniel Nazarian <danielnazarian@outlook.com>
2024-03-23 23:39:52 -0400

    labels.txt

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from transformers import pipeline
 from PIL import Image
 import requests
 
+
+DEFAULT_LABELS_FILE_PATH = "labels.txt"
+
+
+def get_labels_from_file(file_path=DEFAULT_LABELS_FILE_PATH):
+    with open(file_path, "r") as file:
+        return [line.strip() for line in file.readlines()]
+
+
 if __name__ == "__main__":
     model_name = "openai/clip-vit-large-patch14-336"
     classifier = pipeline("zero-shot-image-classification", model=model_name)
@@ -10,12 +19,13 @@ if __name__ == "__main__":
 
     image_to_classify = Image.open(requests.get(url, stream=True).raw)
 
-    labels_for_classification = ["cat and dog", "lion and cheetah", "rabbit and lion"]
+    labels_for_classification = get_labels_from_file()
 
     scores = classifier(image_to_classify, candidate_labels=labels_for_classification)
 
     print("\n=============================")
     print(f"Image URL:\n{url}\n")
-    for obj in scores:
+    for obj in scores[:10]:
         print(f"{obj['label']}: {obj['score']}")
+    print("...")
     print("")
