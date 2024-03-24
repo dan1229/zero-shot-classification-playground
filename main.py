c4e5491d3076cb71226acd34df5267a460416f4e
Daniel Nazarian <danielnazarian@outlook.com>
2024-03-23 23:35:51 -0400

    got working!

---
+++ main.py
@@ -0,0 +1,21 @@
+from transformers import pipeline
+from PIL import Image
+import requests
+
+if __name__ == "__main__":
+    model_name = "openai/clip-vit-large-patch14-336"
+    classifier = pipeline("zero-shot-image-classification", model=model_name)
+
+    url = "https://unsplash.com/photos/g8oS8-82DxI/download?ixid=MnwxMjA3fDB8MXx0b3BpY3x8SnBnNktpZGwtSGt8fHx8fDJ8fDE2NzgxMDYwODc&force=true&w=640"
+
+    image_to_classify = Image.open(requests.get(url, stream=True).raw)
+
+    labels_for_classification = ["cat and dog", "lion and cheetah", "rabbit and lion"]
+
+    scores = classifier(image_to_classify, candidate_labels=labels_for_classification)
+
+    print("\n=============================")
+    print(f"Image URL:\n{url}\n")
+    for obj in scores:
+        print(f"{obj['label']}: {obj['score']}")
+    print("")
